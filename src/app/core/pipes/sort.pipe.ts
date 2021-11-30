fa9d69ccd6baffb93904a24e45f36f048385fe42
armagansenol <armagansnl@gmail.com>
2021-12-01 00:18:51 +0300

    updated sort pipe

--- src/app/core/pipes/sort.pipe.ts
+++ src/app/core/pipes/sort.pipe.ts
@@ -5,28 +5,26 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'sort',
 })
 export class SortPipe implements PipeTransform {
-  transform(items: any[], order: string): any {
+  transform(items: any[], order: string, key: string): any {
     if (!items) {
       return [];
     } else if (!order) {
       return items;
     }
 
-    const sorted = this.sort(items, order);
+    const sorted = this.sortObjectArrayByKey(items, order, key);
     return sorted;
   }
 
-  sort(items: any[], order: string) {
-    if (order === 'ascending') {
-      return items.sort((a: ICampaign, b: ICampaign) => {
-        return a.rating - b.rating;
-      });
-    } else if (order === 'descending') {
-      return items.sort((a: ICampaign, b: ICampaign) => {
-        return b.rating - a.rating;
-      });
-    } else {
-      return items;
-    }
+  sortObjectArrayByKey(arr: any[], order: string, key: string) {
+    return arr.sort((a, b) => {
+      if (order === 'ascending') {
+        return a[key] - b[key];
+      } else if (order === 'descending') {
+        return b[key] - a[key];
+      } else {
+        return 0;
+      }
+    });
   }
 }
